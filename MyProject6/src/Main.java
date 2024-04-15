34a6f47903946baf15b03b475cfd82e3877d0d71
nguyencongthinh-dev <151004961+nguyencongthinh-dev@users.noreply.github.com>
2024-04-15 22:36:48 +0700

    First commit

---
+++ MyProject6/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+	public static int nhap(Scanner input) {
+		boolean check = false;
+		int n = 0;
+		while (!check) {
+			System.out.print(" ");
+			try {
+				n = input.nextInt();
+				check = true;
+			} catch (Exception e) {
+				System.out.println("Ban phai nhap so! hay nhap lai...");
+				input.nextLine();
+			}
+		}
+		return (n);
+	}
+
+	public static int UCLN(int a, int b) {
+		while (a != b) {
+			if (a > b)
+				a = a - b;
+			else
+				b = b - a;
+		}
+		return (a);
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Nhap a");
+		int a = nhap(input);
+		System.out.println("Nhap b");
+		int b = nhap(input);
+		System.out.println("Uoc chung lon nhat cua " + a + " va " + b + " la: " + UCLN(a, b));
+		System.out.println("XONG!");
+	}
+}
